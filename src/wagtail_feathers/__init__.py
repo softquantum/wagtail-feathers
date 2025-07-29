843851afa5c2648bd120e66247ceae975b90ee09
softquantum <maxime@softquantum.com>
2025-07-29 13:19:54 -0400

    added migrations.swappable_dependency for image and document models in case of custom models

--- src/wagtail_feathers/__init__.py
+++ src/wagtail_feathers/__init__.py
@@ -2,6 +2,6 @@ from .version import get_version
 
 
 # release must be one of alpha, beta, rc, or final
-VERSION = (1, 0, 0, "beta", 1)
+VERSION = (1, 0, 0, "beta", 2)
 
 __version__ = get_version(VERSION)
