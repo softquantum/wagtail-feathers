9a5dbb896a3fd0e0abf02849f1ef0d5c917c9967
softquantum <maxime@softquantum.com>
2025-07-29 16:56:49 -0400

    bumping version

--- src/wagtail_feathers/__init__.py
+++ src/wagtail_feathers/__init__.py
@@ -2,6 +2,6 @@ from .version import get_version
 
 
 # release must be one of alpha, beta, rc, or final
-VERSION = (1, 0, 0, "beta", 2)
+VERSION = (1, 0, 0, "beta", 3)
 
 __version__ = get_version(VERSION)
