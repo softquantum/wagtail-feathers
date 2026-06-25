92ac3971252e33786c82d0a563e0b23747aa23c4
softquantum <maxime@softquantum.com>
2026-06-24 21:30:09 -0400

    updated tests env, added a missing migration

--- src/wagtail_feathers/__init__.py
+++ src/wagtail_feathers/__init__.py
@@ -1,7 +1,7 @@
 from .version import get_version
 
 # release must be one of alpha, beta, rc, or final
-VERSION = (1, 0, 1, "final", 0)
+VERSION = (1, 0, 2, "final", 0)
 
 __version__ = get_version(VERSION)
 
